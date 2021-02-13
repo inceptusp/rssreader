6d7bae2b52e700e38f468405c4817ac10c5774f3
Pedro Carvalho <17789155+inceptusp@users.noreply.github.com>
2021-02-13 02:12:42 -0300

    Added PWA capabilities

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+
+  pwa: {
+    name: 'RSS Reader',
+    themeColor: '#F5F5F5',
+    msTileColor: '#F5F5F5',
+    appleMobileWebAppCapable: 'yes',
+    manifestOptions: {
+      background_color: '#FFFFFF',
+      start_url: '/',
+      display: 'standalone',
+      orientation: 'portrait'
+    },
+  }
 }
