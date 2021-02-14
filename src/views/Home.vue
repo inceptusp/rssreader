99c8df6bf270d8f2f89f67f55a6370c82672368d
Pedro Carvalho <17789155+inceptusp@users.noreply.github.com>
2021-02-14 02:41:39 -0300

    Added a Home page with a welcome message

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,9 @@
   <v-container fluid fill-height>
     <v-layout align-center justify-center>
       <v-list>
-        <v-img src="assets/images/appIcon.png" width="256" height="256" />
-        <p>TESTE</p>
+        <v-img src="../assets/images/app_icon.png" width="256" height="256" class="center" />
+        <h3 class="center text-h4">Welcome</h3>
+        <h5 class="center text-subtitle-1">Welcome to our RSS Reader, click on the top left corner to login or sing up so we can start</h5>
       </v-list>
     </v-layout>
   </v-container>
@@ -24,3 +25,12 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+}
+</style>
