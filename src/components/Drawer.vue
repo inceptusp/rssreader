c80131888f3d630e06aa52b0504996309680fb64
Pedro Carvalho <17789155+inceptusp@users.noreply.github.com>
2021-02-15 19:37:14 -0300

    Added icons for future features

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -2,23 +2,78 @@
   <v-navigation-drawer v-model="drawer" app fixed mobile-breakpoint="960">
     <template v-slot:prepend>
       <v-app-bar flat>
-        <v-list-item @click="showAccountDialog()">
-          <v-list-item-avatar>
-            <v-img
+        <v-list-item
+          @click="showAccountDialog()"
+          two-line
+          style="max-height: 64px"
+        >
+          <v-list-item-avatar color="grey">
+            <v-icon color="white">mdi-account</v-icon>
+            <!-- <v-img
               src="https://randomuser.me/api/portraits/women/85.jpg"
-            ></v-img>
+            ></v-img> -->
           </v-list-item-avatar>
-          <v-row style="padding: 8px">
+          <v-list-item-content>
             <v-list-item-title>Welcome</v-list-item-title>
             <v-list-item-subtitle>Login or Sing Up</v-list-item-subtitle>
-          </v-row>
+          </v-list-item-content>
         </v-list-item>
       </v-app-bar>
 
-      <authenticationDialog v-model="accountDialog"/>
+      <authenticationDialog v-model="accountDialog" />
     </template>
 
-    <v-list>
+    <v-list-item>
+      <v-layout align-center justify-center>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on" @click.stop="openSearch()">
+              <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+          </template>
+          <span>Search</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on" @click.stop="">
+              <v-icon>mdi-plus-circle</v-icon>
+            </v-btn>
+          </template>
+          <span>New feed</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on" @click.stop="">
+              <v-icon>mdi-account</v-icon>
+            </v-btn>
+          </template>
+          <span>Account</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on" @click.stop="">
+              <v-icon>mdi-cog</v-icon>
+            </v-btn>
+          </template>
+          <span>Settings</span>
+        </v-tooltip>
+      </v-layout>
+    </v-list-item>
+
+    <v-divider />
+
+    <v-text-field
+      v-if="searchBar"
+      v-model="search"
+      label="Search"
+      outlined
+      hide-details="auto"
+      style="padding: 8px"
+    />
+
+    <v-divider v-if="searchBar" />
+
+    <v-list shaped>
       <v-list-item link to="/">
         <v-list-item-title> Home </v-list-item-title>
       </v-list-item>
@@ -36,13 +91,13 @@
 </template>
 
 <script>
-import AuthenticationDialog from '../components/AuthenticationDialog.vue'
+import AuthenticationDialog from "../components/AuthenticationDialog.vue";
 
 export default {
   name: "Drawer",
 
   components: {
-    authenticationDialog: AuthenticationDialog
+    authenticationDialog: AuthenticationDialog,
   },
 
   props: {
@@ -56,6 +111,8 @@ export default {
   data: () => ({
     drawer: null,
     accountDialog: false,
+    searchBar: false,
+    search: "",
   }),
 
   watch: {
@@ -73,6 +130,18 @@ export default {
       this.drawer = null;
       this.accountDialog = !this.accountDialog;
     },
+
+    openSearch() {
+      this.searchBar = !this.searchBar;
+    },
   },
 };
 </script>
+
+<style>
+@media only screen and (min-width: 960px) {
+  .v-toolbar__content {
+    padding: 0px;
+  }
+}
+</style>
