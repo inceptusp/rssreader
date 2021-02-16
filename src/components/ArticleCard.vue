2fc38e2a95005cb32aae6e58ad4efbca3b659449
Pedro Carvalho <17789155+inceptusp@users.noreply.github.com>
2021-02-16 19:32:32 -0300

    Fixed some issues on article rendering on small screens and added content reader dialog

--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -1,34 +1,82 @@
 <template>
-  <v-card outlined>
-    <v-row align="center" style="padding: 4px 8px">
-      <v-avatar size="128px" class="ma-3" tile>
+  <v-card outlined class="articleCard">
+    <v-list-item style="padding: 2px 16px">
+      <v-list-item-avatar tile size="auto">
         <v-flex contain>
           <v-img v-bind:src="article.image" />
         </v-flex>
-      </v-avatar>
-      <v-col>
-        <v-card-title style="word-break: normal">{{
-          article.title
-        }}</v-card-title>
-        <v-card-subtitle
-          v-html="article.description"
-          style="text-align: justify"
-        />
-      </v-col>
-      <v-btn icon v-bind:href="'https://outline.com/' + article.url" large>
-        <v-icon>mdi-book-open</v-icon>
-      </v-btn>
-      <v-container style="width: 16px;"/>
-    </v-row>
+      </v-list-item-avatar>
+      <v-card-title style="word-break: normal; text-align: justify">
+        {{ article.title }}
+      </v-card-title>
+    </v-list-item>
+    <v-card-subtitle v-html="article.description" style="text-align: justify" />
+    <v-card-actions>
+      <v-layout align-center justify-center>
+        <v-tooltip bottom v-if="article.content != ''">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on" @click.stop="showFullArticle" large>
+              <v-icon>mdi-book-open-variant</v-icon>
+            </v-btn>
+          </template>
+          <span>{{ $t("Read full article") }}</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              icon
+              v-bind="attrs"
+              v-on="on"
+              v-bind:href="'https://outline.com/' + article.url"
+              large
+            >
+              <v-icon>mdi-text</v-icon>
+            </v-btn>
+          </template>
+          <span>{{ $t("Outline") }}</span>
+        </v-tooltip>
+      </v-layout>
+    </v-card-actions>
+
+    <fullArticleDialog
+      v-model="showArticle"
+      v-bind:title="article.title"
+      v-bind:content="article.content"
+    />
   </v-card>
 </template>
 
 <script>
+import FullArticleDialog from "../components/FullArticleDialog";
+
 export default {
   name: "ArticleCard",
 
+  components: {
+    fullArticleDialog: FullArticleDialog,
+  },
+
   props: {
     article: { type: Object },
+  },
+
+  mounted: function () {
+    var aElements = document.getElementsByClassName("articleCard");
+    aElements.forEach(function (el) {
+      el.getElementsByTagName("a").forEach(function (e) {
+        e.setAttribute("target", "_blank");
+      });
+    });
+  },
+
+  data: () => ({
+    showArticle: false,
+  }),
+
+  methods: {
+    showFullArticle() {
+      this.showArticle = !this.showArticle;
+    },
   },
 };
 </script>
---
+++ src/components/FullArticleDialog.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-dialog v-model="dialog" max-width="800" scrollable fullscreen>
+    <v-card>
+      <v-card-title>
+        <v-btn icon @click="dialog = false">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+        <div style="padding-left: 8px;">{{ title }}</div>
+        <v-spacer />
+        <v-btn icon @click="dialog = false">
+          <v-icon>mdi-share-variant</v-icon>
+        </v-btn>
+      </v-card-title>
+      <v-divider />
+      <v-card-text
+        style="text-align: justify; padding-top: 16px"
+        v-html="content"
+      />
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "FullArticleDialog",
+
+  props: {
+    title: { type: String },
+    content: { type: String },
+    value: { type: Boolean },
+  },
+
+  mounted: function () {
+    this.dialog = this.value;
+  },
+
+  data: () => ({
+    dialog: false,
+  }),
+
+  watch: {
+    value: function () {
+      this.dialog = this.value;
+    },
+
+    dialog: function () {
+      this.$emit("input", this.dialog);
+    },
+  },
+};
+</script>
