inceptusp/rssreader

commit b786c724705645672863d331f8197c3c4ee0a94d

Changed empty strings to null

src/views/Home.vue
@@ -10,11 +10,11 @@
         />
         <h3 class="center text-h4">{{ $t("Welcome") + " " + userName }}</h3>
         <h5 class="center text-subtitle-1">
-          <div v-if="userName == ''" id="welcome-subtitle">
+          <div v-if="userName == null" id="welcome-subtitle">
             {{ $t("Welcome to RSS Reader, click on the top left corner to login or sign up so we can start") }}
           </div>
           <div
-            v-else-if="userName != '' && feedListLenght == 0"
+            v-else-if="userName != null && feedListLenght == 0"
             id="welcome-subtitle"
           >
             {{ $t("Welcome to RSS Reader, click on the + button to add a new feed") }}
@@ -43,13 +43,13 @@ export default {
   },
 
   data: () => ({
-    userName: "",
+    userName: null,
     feedListLenght: 0,
   }),
 
   methods: {
     sendFeedNameToAppBar() {
-      this.$emit("feedName", "");
+      this.$emit("feedName", null);
     },
   },
 };

src/App.vue
@@ -50,7 +50,7 @@ export default {
 
   data: () => ({
     drawer: null,
-    feedName: "",
+    feedName: null,
   }),
 
   methods: {

src/components/AppBar.vue
@@ -3,7 +3,7 @@
     <v-app-bar-nav-icon @click.stop="drawerControl" class="hidden-md-and-up" />
     <v-toolbar-title>
       {{ $t("RSS Reader") }}
-      <div v-if="feedName != ''" style="display: inline">-</div>
+      <div v-if="feedName != null" style="display: inline">-</div>
       {{ feedName }}
     </v-toolbar-title>
     <v-spacer />
@@ -52,8 +52,8 @@ export default {
 
   data: () => ({
     showAbout: false,
-    aboutTitle: "",
-    aboutContent: "",
+    aboutTitle: null,
+    aboutContent: null,
   }),
 
   methods: {

src/components/ArticleCard.vue
@@ -13,7 +13,7 @@
     <v-card-subtitle v-html="article.description" style="text-align: justify" />
     <v-card-actions>
       <v-layout align-center justify-center>
-        <v-tooltip bottom v-if="article.content != ''">
+        <v-tooltip bottom v-if="article.content != null">
           <template v-slot:activator="{ on, attrs }">
             <v-btn icon v-bind="attrs" v-on="on" @click.stop="showFullArticle" large>
               <v-icon>mdi-book-open-variant</v-icon>

src/components/SettingsDialog.vue
@@ -11,7 +11,7 @@
         <v-select
           v-bind:items="feedsOrderOptions"
           v-bind:label="
-            $vuetify.breakpoint.width < 960 ? $t('Order feeds list') : ''
+            $vuetify.breakpoint.width < 960 ? $t('Order feeds list') : null
           "
           outlined
           hide-details

src/views/FeedArticleList.vue
@@ -42,7 +42,7 @@ export default {
                 .childNodes[0].nodeValue,
               content:
                 element.getElementsByTagName("content:encoded").length == 0
-                  ? ""
+                  ? null
                   : element.getElementsByTagName("content:encoded")[0]
                       .childNodes[0].nodeValue,
               url: element.getElementsByTagName("link")[0].childNodes[0]

commit 6782cdbd3a00335711a54533454265cd3cb8ed7f

Fixed bugs caused by previous commit (b786c724705645672863d331f8197c3c4ee0a94d)

src/components/Drawer.vue
@@ -8,7 +8,7 @@
           style="max-height: 64px"
         >
           <v-list-item-avatar color="grey">
-            <v-icon color="white" v-if="userPhoto == ''">mdi-account</v-icon>
+            <v-icon color="white" v-if="userPhoto == null">mdi-account</v-icon>
             <v-img v-else>
               <img v-bind:src="userPhoto" referrerpolicy="no-referrer" />
             </v-img>

src/views/Home.vue
@@ -8,7 +8,7 @@
           height="256"
           class="center"
         />
-        <h3 class="center text-h4">{{ $t("Welcome") + " " + userName }}</h3>
+        <h3 class="center text-h4">{{ $t("Welcome") + " " + userName == null ? "" : userName }}</h3>
         <h5 class="center text-subtitle-1">
           <div v-if="userName == null" id="welcome-subtitle">
             {{ $t("Welcome to RSS Reader, click on the top left corner to login or sign up so we can start") }}