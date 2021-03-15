5662ec209030ad9a7ee185392742ac52a20d75df
Pedro Carvalho <17789155+inceptusp@users.noreply.github.com>
2021-03-15 06:03:19 -0300

    Added signout on server logic

--- src/components/LogOffDialog.vue
+++ src/components/LogOffDialog.vue
@@ -3,31 +3,41 @@
     <v-card>
       <v-card-title class="headline">{{ $t("Are you sure?") }}</v-card-title>
 
-      <v-card-text>{{ $t("Are you sure that you want to sign out?") }}</v-card-text>
+      <v-card-text>{{
+        $t("Are you sure that you want to sign out?")
+      }}</v-card-text>
 
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn
-          color="#00bfa5"
-          text
-          v-on:click="dialog = false;"
-        >{{ $t("Close") }}</v-btn>
-        <v-btn
-          color="#00bfa5"
-          text
-          @click="signOut()"
-        >{{ $t("Sign out") }}</v-btn>
+        <v-btn color="#00bfa5" text v-on:click="dialog = false">{{
+          $t("Close")
+        }}</v-btn>
+        <v-btn color="#00bfa5" text @click="signOut()">{{
+          $t("Sign out")
+        }}</v-btn>
       </v-card-actions>
     </v-card>
+
+    <alert-dialog
+      v-model="errorDialog"
+      v-bind:title="errorTitle"
+      v-bind:content="errorContent"
+    />
   </v-dialog>
 </template>
 
 <script>
+import AlertDialog from "../components/AlertDialog";
+
 export default {
   name: "AlertDialog",
 
+  components: {
+    AlertDialog,
+  },
+
   props: {
-    value: { type: Boolean }
+    value: { type: Boolean },
   },
 
   mounted: function () {
@@ -35,11 +45,42 @@ export default {
   },
 
   data: () => ({
-    dialog: false
+    dialog: false,
+    errorDialog: false,
+    errorTitle: "",
+    errorContent: "",
   }),
 
   methods: {
     signOut() {
+      var selfVue = this;
+
+      var obj = new Object();
+      obj.email = window.localStorage.getItem("email");
+      obj.uuid = window.localStorage.getItem("uuid");
+      var jsonString = JSON.stringify(obj);
+
+      var connection = new WebSocket(
+        "wss://rssreader.aplikoj.com/wss/",
+        "PDRAUM"
+      );
+      connection.onerror = function (error) {
+        selfVue.errorTitle = "Comunication error";
+        selfVue.errorContent =
+          '<p>There was a communication error with the server and/or the internet. Check your connection or try again later.</p><p style="opacity: 0.8">Error code: ' +
+          "websocket_" +
+          error.type +
+          "</p>";
+        selfVue.showErrorDialog();
+      };
+      connection.onopen = function () {
+        var byte = new Uint8Array(1);
+        byte[0] = 0x04;
+        connection.send("105 ");
+        connection.send(jsonString);
+        connection.send(byte);
+      };
+      connection.onclose = function () {
         window.localStorage.removeItem("user");
         window.localStorage.removeItem("email");
         window.localStorage.removeItem("pic");
@@ -47,10 +88,17 @@ export default {
         window.localStorage.removeItem("uuid");
         window.localStorage.removeItem("feeds");
         window.localStorage.removeItem("settings");
-        window.gapi.auth2.getAuthInstance().signOut();
+        if (window.gapi.auth2.getAuthInstance().isSignedIn) {
+          window.gapi.auth2.getAuthInstance().signOut();
+        }
         this.dialog = false;
         document.location.replace(window.location.origin);
-    }
+      };
+    },
+
+    showErrorDialog() {
+      this.errorDialog = !this.errorDialog;
+    },
   },
 
   watch: {
