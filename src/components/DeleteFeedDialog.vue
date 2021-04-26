308821f2e42f0fe4571fa0286d4807b621c72072
Pedro Carvalho <17789155+inceptusp@users.noreply.github.com>
2021-04-26 06:42:47 -0300

    Finished delete feed function

---
+++ src/components/DeleteFeedDialog.vue
@@ -0,0 +1,113 @@
+<template>
+  <v-dialog v-model="dialog" max-width="320">
+    <v-card>
+      <v-card-title>{{ $t("Are you sure?") }}</v-card-title>
+
+      <v-card-text>{{ $t("Are you sure that you want to delete this feed?") }}</v-card-text>
+
+      <v-card-actions v-if="!sending">
+        <v-spacer></v-spacer>
+        <v-btn color="#00bfa5" text v-on:click="dialog = false">{{
+          $t("Cancel")
+        }}</v-btn
+        ><v-btn color="#00bfa5" text v-on:click="removeFeed()">{{
+          $t("Confirm")
+        }}</v-btn>
+      </v-card-actions>
+      <v-card-actions v-else>
+        <v-spacer></v-spacer>
+        <v-progress-circular indeterminate color="#00bfa5" />
+      </v-card-actions>
+    </v-card>
+
+    <alert-dialog
+      v-model="errorDialog"
+      v-bind:title="errorTitle"
+      v-bind:content="errorContent"
+    />
+  </v-dialog>
+</template>
+
+<script>
+import AlertDialog from "../components/AlertDialog";
+import websocketHelper from "../websocketHelper";
+import { errorMessages } from "../errorMessages";
+
+export default {
+  name: "DeleteFeedDialog",
+
+  components: {
+    AlertDialog,
+  },
+
+  props: {
+    value: { type: Boolean },
+    feedAddress: { type: String },
+  },
+
+  mounted: function () {
+    this.dialog = this.value;
+  },
+
+  data: () => ({
+    dialog: false,
+    sending: false,
+    errorDialog: false,
+    errorTitle: null,
+    errorContent: null,
+  }),
+
+  methods: {
+    showErrorDialog() {
+      this.errorDialog = !this.errorDialog;
+    },
+
+    removeFeed() {
+      const selfVue = this;
+      
+      var obj = new Object();
+      obj.feedAddress = selfVue.feedAddress;
+      obj.variable = window.localStorage.getItem("l");
+      obj.uuid = window.localStorage.getItem("sid");
+    
+      var jsonString = JSON.stringify(obj);
+
+      var connection = websocketHelper.rssReaderWs();
+      connection.onerror = function (error) {
+        websocketHelper.onError(error, selfVue);
+        selfVue.sending = false;
+      }
+      connection.onopen = function () {
+        selfVue.sending = true;
+        var byte = new Uint8Array(1);
+        byte[0] = 0x04;
+        connection.send("305 ");
+        connection.send(jsonString);
+        connection.send(byte);
+      };
+      connection.onmessage = function (msg) {
+        var response = JSON.parse(msg.data);
+        if (Object.prototype.hasOwnProperty.call(response, "error")) {
+          errorMessages(response.error, selfVue);
+        } else {
+          selfVue.dialog = false;
+          selfVue.sending = false;
+        }
+      }
+    },
+  },
+
+  watch: {
+    value: function () {
+      this.dialog = this.value;
+    },
+
+    dialog: function () {
+      if (this.$refs.formRef != undefined) {
+        this.$refs.formRef.reset();
+      }
+      this.$emit("input", this.dialog);
+    },
+  },
+};
+</script>
