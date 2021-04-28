26b3b845b6924a1c358fc580a9c7615edfce93c1
Pedro Carvalho <17789155+inceptusp@users.noreply.github.com>
2021-04-28 00:32:49 -0300

    Changed some var names to match the default from templates

--- src/errorMessages.js
+++ src/errorMessages.js
@@ -1,8 +1,8 @@
 export function errorMessages(errorCode, selfVue) {
     switch (errorCode) {
         case "json_with_error":
-            selfVue.alertTitle = "Erro de transmissão";
-            selfVue.alertContent =
+            selfVue.errorTitle = "Erro de transmissão";
+            selfVue.errorContent =
             '<p>Houve um erro na transmissão, verifique o link que você recebeu e tente novamente.</p><p style="opacity: 0.8">Código de erro: ' +
             errorCode +
             "</p>";
@@ -10,8 +10,8 @@ export function errorMessages(errorCode, selfVue) {
             selfVue.sending = false;
             break;
         case "user_not_found":
-            selfVue.alertTitle = "Usuário não encontrado";
-            selfVue.alertContent =
+            selfVue.errorTitle = "Usuário não encontrado";
+            selfVue.errorContent =
             '<p>Este usuário não foi encontrado em nosso sistema Verifique o link que você recebeu e tente novamente.</p><p style="opacity: 0.8">Código de erro: ' +
             errorCode +
             "</p>";
@@ -19,8 +19,8 @@ export function errorMessages(errorCode, selfVue) {
             selfVue.sending = false;
             break;
         case "exception_empty_confid" || "wrong_confid":
-            selfVue.alertTitle = "Erro com a chave de verificação";
-            selfVue.alertContent =
+            selfVue.errorTitle = "Erro com a chave de verificação";
+            selfVue.errorContent =
             '<p>Houve um erro ao confirmar a chave de verificação, verifique o link que você recebeu e tente novamente.</p><p style="opacity: 0.8">Código de erro: ' +
             errorCode +
             "</p>";
@@ -28,8 +28,8 @@ export function errorMessages(errorCode, selfVue) {
             selfVue.sending = false;
             break;
         default:
-            selfVue.alertTitle = "Erro desconhecido";
-            selfVue.alertContent =
+            selfVue.errorTitle = "Erro desconhecido";
+            selfVue.errorContent =
             '<p>Ops! Você acabou de esbarrar em um erro que ainda não conhecemos, por favor, entre em contato conosco informando o código de erro abaixo para que possamos corrigí-lo.</p><p style="opacity: 0.8">Código de erro: ' +
             errorCode +
             "</p>";
